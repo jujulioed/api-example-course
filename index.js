7fde6b443728c1aa22915a28563b15cce34c7174
juujulio <julioeduardon9@hotmail.com>
2024-08-28 23:20:21 -0300

    Get data from different fonts
    	- Created endpoint that get data from Query params;
    	- Created endpoint that get data from Route params.
    
    Signed-off-by: juujulio <julioeduardon9@hotmail.com>

--- index.js
+++ index.js
@@ -4,10 +4,24 @@ const express = require('express');
 // Instantiating the server
 const server = express();
 
+// Query params: params that are passed in the route. Example: ?name=NodeJS
+// Route params: params that are passed directly in the route. Example: /route/2
+// Request Body: { name: "NodeJS", type: "Backend" }
+
 // Creating the first endpoint
 // localhost:3000/route
-server.get('/route', (req, res) => {
-    return res.json({ route: "NodeJS route" })
+server.get('/firstroute', (req, res) => {
+    // Getting data using Query params
+    const name = req.query.name;
+
+    return res.json({ route: `Showing name: ${name}` })
+})
+
+server.get('/secondroute/:id', (req, res) => {
+    // Getting data using Route params
+    const id = req.params.id;
+
+    return res.json({ route: `Showing id: ${id}` })
 })
 
 server.listen(3000);
